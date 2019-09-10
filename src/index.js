9a5f17f313e7557c46119f759451e6bce6f3e76b
Beigenut <beigenut@gmail.com>
2019-09-10 16:37:45 -0400

    Set to use Async / Await

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert(`${process.env.TEST_TXT}!!!`)
+import axios from "axios";
+// import 'babel-polyfill'; Incase of older browsers
+
+const instance = axios.create({
+  baseURL: process.env.API_URL
+});
+
+const inserter = document.querySelector('.insert');
+
+const insert = async () => {
+  const res = await instance.get(`/users`)
+  res.data.forEach(i => {
+    inserter.textContent = i.username
+  });
+}
+
+insert()
